c8305775b3f78464e8f15c42eb9dfccefae67a24
ggopichandu <gummagopichand@gmail.com>
2024-08-31 18:10:50 +0530

    hello script

---
+++ 06-array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MOVIES=("Dhruva" "DJ" "Khaleja" "Ismart")
+
+# size of above array is 4.
+# index are 0,1,2,3
+
+# list always starts with 0.
+
+echo "Frist Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[1]}"
+echo "First Movie is: ${MOVIEs[2]}"
+echo "First Movie is: ${MOVIES[3]}"
