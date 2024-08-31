7f3e9898ea83ddf42475e1dc67f49448d8e0488b
ggopichandu <gummagopichand@gmail.com>
2024-08-31 19:18:45 +0530

    hello script

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,7 @@
 
 echo "All variables: $@"
 echo "Number of variables passed: $#"
+echo "Name of script: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
