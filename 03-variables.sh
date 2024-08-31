d56175c93fa0e373e96ec9cf5397afae144c7b00
ggopichandu <gummagopichand@gmail.com>
2024-08-31 17:34:54 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramu
+PERSON2=Gopi
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How about you?"
+echo "$PERSON1:: I am fine. I am interested to learn DevOps."
+echo "$PERSON2:: Hey that's good idea"
