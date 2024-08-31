27fc614344b7b30697099348ba91acbcc001e7c8
ggopichandu <gummagopichand@gmail.com>
2024-08-31 18:11:42 +0530

    hello script

--- 06-array.sh
+++ 06-array.sh
@@ -9,5 +9,5 @@ MOVIES=("Dhruva" "DJ" "Khaleja" "Ismart")
 
 echo "Frist Movie is: ${MOVIES[0]}"
 echo "First Movie is: ${MOVIES[1]}"
-echo "First Movie is: ${MOVIEs[2]}"
+echo "First Movie is: ${MOVIES[2]}"
 echo "First Movie is: ${MOVIES[3]}"
